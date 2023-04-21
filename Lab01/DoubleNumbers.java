9001fcc9ab1eb485b70e32fbc4b1867b12de9301
hwng and hwng <91714479+hwnginsoict@users.noreply.github.com>
2023-04-21 17:04:31 +0700

    Add files via upload

---
+++ Lab01/DoubleNumbers.java
@@ -0,0 +1,33 @@
+import java.lang.Math;
+import java.util.Scanner;
+
+public class DoubleNumbers {
+	public static double sum(double a, double b) {
+		return a + b;
+	}
+	public static double dif(double a, double b) {
+		return Math.abs(a-b);
+	}
+	public static double pdt(double a, double b) {
+		return a * b;
+	}
+	public static double qut(double a, double b) {
+		if (b == 0.0) {
+			throw new IllegalArgumentException("Error");
+		}
+		return a/b;
+	}
+	
+	public static void main(String[] args) {
+		Scanner keyboard = new Scanner(System.in);
+		System.out.print("Enter first number: ");
+		double a = keyboard.nextDouble();
+		System.out.print("Enter second number: ");
+		double b = keyboard.nextDouble();
+		keyboard.close();
+		System.out.println("Sum: " + sum(a,b));
+		System.out.println("Difference: " + dif(a,b));
+		System.out.println("Product: " + pdt(a,b));
+		System.out.println("Quotient: " + qut(a,b));
+	}
+}
