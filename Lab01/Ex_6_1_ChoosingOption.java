5efb7ff354f3e55052ed83639a6d17b456eac558
hwng and hwng <91714479+hwnginsoict@users.noreply.github.com>
2023-04-22 10:44:10 +0700

    Update Ex_6_1_ChoosingOption.java

--- Lab01/Ex_6_1_ChoosingOption.java
+++ Lab01/Ex_6_1_ChoosingOption.java
@@ -8,5 +8,17 @@ public class Ex_6_1_ChoosingOption {
 		JOptionPane.showMessageDialog(null, "You've chosen: " 
 				+ (option==JOptionPane.YES_OPTION?"Yes":"No"));
 		System.exit(0);
+
+		// Answer:
+		// - If choose "Cancel", the dialog will return "You've chosen: No" 
+		// - If you want to show 2 options, you should use JOptionPane.showOptionDialog:
+		String[] options = {"I do", "I don't"}; 
+			JOptionPane.showOptionDialog(null,"Do you want to change to the first class ticket?",
+			"Choose",
+			JOptionPane.DEFAULT_OPTION, 
+			JOptionPane.INFORMATION_MESSAGE,
+			null,
+			options, 
+			options[0]); 
 	}
 }
