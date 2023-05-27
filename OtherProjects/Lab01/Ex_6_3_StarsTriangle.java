d4ed69d078390e49711b3062934ee693c1317ecd
hwng and hwng <91714479+hwnginsoict@users.noreply.github.com>
2023-05-27 08:49:11 +0700

    Add files via upload

---
+++ OtherProjects/Lab01/Ex_6_3_StarsTriangle.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+//import javax.swing.JOptionPane;
+
+public class Ex_6_3_StarsTriangle {
+	public static void main(String[] args){
+		Scanner keyboard = new Scanner(System.in);
+        System.out.print("Enter n: ");
+		int n = keyboard.nextInt();
+		keyboard.close();
+//		String obj;
+//		obj = JOptionPane.showInputDialog("Number:");
+//		int n = Integer.parseInt(obj);
+		for (int i=1; i<=n; i++) {
+			for(int j=0; j<n-i; j++) {
+				System.out.print(" ");
+			}
+			for(int j=0; j<2*i-1; j++){
+				System.out.print("*");
+			}
+			for(int j=0; j<n-i; j++) {
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}
+}
