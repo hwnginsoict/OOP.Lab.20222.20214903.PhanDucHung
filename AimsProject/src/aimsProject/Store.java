dbbcd1f6774e4650c9a3b7e5cca0a53dd29409a7
hwng and hwng <91714479+hwnginsoict@users.noreply.github.com>
2023-06-05 08:42:09 +0700

    Add files via upload

---
+++ AimsProject/src/aimsProject/Store.java
@@ -0,0 +1,27 @@
+package hust.soict.dsai.aims.store;
+import java.util.ArrayList;
+import hust.soict.dsai.aims.disc.DigitalVideoDisc;
+
+public class Store {
+	private ArrayList<DigitalVideoDisc> itemsInStore = new ArrayList<DigitalVideoDisc>();
+	private int nbItems = 0;
+	public void addDVD(DigitalVideoDisc dvd) {
+		itemsInStore.add(dvd);
+		nbItems += 1;
+		System.out.println("The disc is added to store!");
+	}
+	public void removeDVD(DigitalVideoDisc dvd) {
+		try {
+			itemsInStore.remove(dvd);
+			nbItems -= 1;
+			System.out.println("The disc is removed from store!");
+		}
+		catch (Exception e) {
+			System.out.println("Disc doesn't exist!");
+			return ;
+		}
+	}
+	public int getNbItems() {
+		return nbItems;
+	}
+}
