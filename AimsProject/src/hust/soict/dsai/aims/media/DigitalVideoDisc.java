f89e70d3f259abce1e3dc777b17aa777c04f76f7
hwng and hwng <91714479+hwnginsoict@users.noreply.github.com>
2023-06-25 05:22:52 +0700

    Update DigitalVideoDisc.java

--- AimsProject/src/hust/soict/dsai/aims/media/DigitalVideoDisc.java
+++ AimsProject/src/hust/soict/dsai/aims/media/DigitalVideoDisc.java
@@ -1,6 +1,6 @@
 package hust.soict.dsai.aims.media;
 
-public class DigitalVideoDisc extends Disc {
+public class DigitalVideoDisc extends Disc implements Playable{
 	
 	public DigitalVideoDisc(String title, String category, String director, int length, float cost){
 		super(title, category, director, length, cost);
